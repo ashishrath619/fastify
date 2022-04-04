78ffa2168847ee3342c7fea088145e9309943737
ashishrath619 <75829394+ashishrath619@users.noreply.github.com>
2022-04-04 10:48:17 +0530

    initial

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export default {
+  "development": {
+    "database": {
+      "client": "mysql",
+      "useNullAsDefault": true,
+      "connection": {
+        "host": 'localhost',
+        "user": 'root',
+        "password": '1234',
+        "database": 'usertable',
+        "port": "3306"
+      }
+    }
+  }
+}
